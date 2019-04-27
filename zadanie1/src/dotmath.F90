f2c2c95fe857f6a8ccf5b69b342aa672ab0eaf38
meehaw1337 <15misiek12@wp.pl>
2019-04-27 17:36:24 +0200

    Dot multiplication added

---
+++ zadanie1/src/dotmath.F90
@@ -0,0 +1,62 @@
+module dotmath
+    implicit none
+    private
+
+    public :: dotmull
+
+    interface dotmull
+        procedure dotmull4, dotmull8, dotmull16
+    end interface
+
+    contains
+
+    function dotmull4 (A, B) result (C)
+        real (kind = 4), intent(in), dimension(:,:) :: A,B
+        real (kind = 4), dimension(size(A,1) , size(B,2)) :: C
+        integer :: i,j
+
+        C=0
+
+        do i=1,size(A,1)
+            do j=1,size(B,2)
+                    C(i,j) = dot_product( A(i,:) , B(:,j) )
+            end do
+        end do 
+
+    end function 
+
+
+    function dotmull8 (A, B) result (C)
+        real (kind = 8), intent(in), dimension(:,:) :: A,B
+        real (kind = 8), dimension(size(A,1) , size(B,2)) :: C
+        integer :: i,j
+
+        C=0
+
+        do i=1,size(A,1)
+            do j=1,size(B,2)
+                    C(i,j) = dot_product( A(i,:) , B(:,j) )
+            end do
+        end do  
+
+    end function
+
+
+    function dotmull16 (A, B) result (C)
+        implicit none
+        real (kind = 16), intent(in), dimension(:,:) :: A,B
+        real (kind = 16), dimension(size(A,1) , size(B,2)) :: C
+        integer :: i,j
+
+        C=0
+
+        do i=1,size(A,1)
+            do j=1,size(B,2)
+                    C(i,j) = dot_product( A(i,:) , B(:,j) )
+            end do
+        end do  
+
+    end function
+
+
+end module dotmath
